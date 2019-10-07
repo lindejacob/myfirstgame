a4ff7b9e9aafe3a50b74f18da7346ba87c7ebb5a
Lindejacob <lindejacob0612@gmail.com>
2019-10-07 17:50:27 +0200

    I am working on the camera, that should follow the Player around

--- Game/Assets/CameraController.cs
+++ Game/Assets/CameraController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public GameObject Player;
+    public Transform target;
+    public float Camspeed = 0.125f;
+    public Vector3 offset;
     // Start is called before the first frame update
     public void Start()
     {  
     }
 
     // Update is called once per frame
-    public void Update()
+    void FixedUpdate ()
     {
+        Vector3 DesiredPosition = GameObject.transform + offset;
+        Vector3 CameraPosition = Vector3.Lerp(GameObject.transform, DesiredPosition, Camspeed*Time.deltaTime); 
 		transform.LookAt(Player.transform);
     }
 }
